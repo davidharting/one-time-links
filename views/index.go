fe8efd58e38a381f265918121c38c453f7374146
David Harting <david.e.harting@gmail.com>
2021-03-18 20:53:02 -0400

    add hacky notice / alert

--- views/index.go
+++ views/index.go
@@ -7,6 +7,11 @@ import (
 	"text/template"
 )
 
+type LayoutProps struct {
+	alert  string
+	notice string
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	log.SetPrefix("view Home\t")
 	if r.Method == http.MethodGet {
@@ -23,13 +28,13 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func HomeIndex(w http.ResponseWriter, r *http.Request) {
 	log.SetPrefix("view HomeIndex\t")
-	t, err := template.ParseFiles("templates/layout.gohtml", "templates/index.gohtml")
+
+	err := render(w, "index", LayoutProps{})
+
 	if err != nil {
 		log.Default().Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Server error"))
-	} else {
-		t.Execute(w, nil)
 	}
 
 }
@@ -38,4 +43,32 @@ func MessageCreate(w http.ResponseWriter, r *http.Request) {
 	log.SetPrefix("view MessageCreate\t")
 	log.Println(fmt.Sprintf("Received form submission, message=%v", r.FormValue("message")))
 	w.Write([]byte(r.FormValue("message")))
+}
+
+type TemplateData struct {
+	Alert     string
+	HasNotice bool
+	HasAlert  bool
+	Notice    string
+}
+
+func render(w http.ResponseWriter, templateName string, props LayoutProps) error {
+	data := getTemplateData(props)
+	t, err := template.ParseFiles("templates/layout.gohtml", fmt.Sprintf("templates/%v.gohtml", templateName))
+	if err != nil {
+		return err
+	}
+	err = t.Execute(w, data)
+	return err
+}
+
+func getTemplateData(props LayoutProps) TemplateData {
+	hasNotice := len(props.notice) > 0
+	hasAlert := len(props.alert) > 0
+	return TemplateData{
+		Alert:     fmt.Sprintf(props.alert),
+		HasAlert:  hasAlert,
+		HasNotice: hasNotice,
+		Notice:    props.notice,
+	}
 }
